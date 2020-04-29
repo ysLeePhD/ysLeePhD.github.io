fa9190ba0fe252ba85e156a80e3ee5210470af0c
ylee366 <yongsung.lee@gatech.edu>
2020-04-29 13:28:41 -0400

    Update blogdown_setup.R

--- blogdown_setup.R
+++ blogdown_setup.R
@@ -1,3 +1,4 @@
+
 # if (!requireNamespace("devtools")) install.packages('devtools')
 # devtools::install_github('rstudio/blogdown')
 
@@ -8,8 +9,9 @@ blogdown::update_hugo()
 # blogdown::new_site(theme = 'gcushen/hugo-academic')
 blogdown::serve_site() # LiveReload
 
+options(blogdown.ext = '.Rmd', blogdown.author = 'John Doe')
+blogdown::new_post()
+
 # 301 Redirect: The Most Important Technique in Maintaining Websites
 # https://yihui.org/en/2017/11/301-redirect/
 
-options(blogdown.ext = '.Rmd', blogdown.author = 'John Doe')
-blogdown::new_post()
